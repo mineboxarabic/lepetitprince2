0d78b497d7de7b1f50b33c1dd8047e8eca2fb51c
Yassin YOUNES <mineboxarabic@gmail.com>
2023-03-20 16:37:14 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lepetitprince2',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
